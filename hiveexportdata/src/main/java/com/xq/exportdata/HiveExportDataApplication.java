71e41cacd163f92028d067af80075c9e9d9a2e85
youjingdike <82329839@qq.com>
2019-09-10 16:29:10 +0800

    提交打zip包的配置

--- hiveexportdata/src/main/java/com/xq/exportdata/HiveExportDataApplication.java
+++ hiveexportdata/src/main/java/com/xq/exportdata/HiveExportDataApplication.java
@@ -2,6 +2,7 @@ package com.xq.exportdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,6 +12,12 @@ public class HiveExportDataApplication extends SpringBootServletInitializer {
         SpringApplication.run(HiveExportDataApplication.class, args);
 
     }
+
+    //如果部署到容器需要这么修改
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }*/
 }
 
 
