9b12af3843cc291e287e6fedd94813e0484a5bc0
youjingdike <82329839@qq.com>
2019-12-05 17:17:53 +0800

    提交新代码

--- nio-netty/src/main/java/com/xq/nio/BufferFillDrain.java
+++ nio-netty/src/main/java/com/xq/nio/BufferFillDrain.java
@@ -9,6 +9,7 @@ import java.nio.CharBuffer;
 public class BufferFillDrain {
     private static int index = 0;
     private static String[] strArr = {
+            "abcdefg",
             "A random string value",
             "tejljej",
             "jsjfjkjljl",
@@ -18,6 +19,22 @@ public class BufferFillDrain {
     };
 
     private static void drainBuffer(CharBuffer charBuffer) {
+        while (charBuffer.hasRemaining()) {
+            System.out.println(charBuffer.get());
+        }
+        System.out.println("@@@@@@@@@@@@@");
+    }
+
+    private static void compactBuffer(CharBuffer charBuffer) {
+        charBuffer.flip();
+        int remaining = charBuffer.remaining();
+        int num = remaining/2;
+        for (int i=0;i<num-1;i++) {
+            System.out.println(charBuffer.get());
+        }
+        charBuffer.compact();
+        System.out.println("!!!!!!!!");
+        charBuffer.flip();
         while (charBuffer.hasRemaining()) {
             System.out.println(charBuffer.get());
         }
@@ -38,11 +55,12 @@ public class BufferFillDrain {
 
     public static void main( String[] args ) {
         CharBuffer charBuffer = CharBuffer.allocate(100);
-        while (fillBuffer(charBuffer)) {
+        /*while (fillBuffer(charBuffer)) {
             charBuffer.flip();
             drainBuffer(charBuffer);
             charBuffer.clear();
-        }
-
+        }*/
+        fillBuffer(charBuffer);
+        compactBuffer(charBuffer);
     }
 }
