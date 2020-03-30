ea08431c7772d7778be058e092da69eb8e7f0020
youjingdike <82329839@qq.com>
2020-03-31 01:07:54 +0800

    提交新代码

---
+++ nio-netty/src/main/java/com/xq/netty/timeserver1/TimeServerHandler.java
@@ -0,0 +1,35 @@
+package com.xq.netty.timeserver1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("The time server receive order:" + body + ",the counter is :" + ++counter);
+
+        String curTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAND ORDER";
+        curTime = curTime + System.getProperty("line.separator");
+        System.out.println("curTime"+curTime);
+        ByteBuf resp = Unpooled.copiedBuffer(curTime.getBytes());
+        ctx.write(resp);
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
