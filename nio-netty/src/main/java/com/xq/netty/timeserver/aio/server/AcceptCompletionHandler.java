00330a44d7abd5f5b477a4a788baec983717d75c
youjingdike <82329839@qq.com>
2020-06-03 16:41:05 +0800

    提交新代码

--- nio-netty/src/main/java/com/xq/netty/timeserver/aio/server/AcceptCompletionHandler.java
+++ nio-netty/src/main/java/com/xq/netty/timeserver/aio/server/AcceptCompletionHandler.java
@@ -10,6 +10,12 @@ public class AcceptCompletionHandler implements CompletionHandler<AsynchronousSo
     public void completed(AsynchronousSocketChannel result, AsyncTimeServerHandler attachment) {
         attachment.asynchronousServerSocketChannel.accept(attachment,this);
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        /*
+        * read的三个参数的含义：
+        * ByteBuffer dst：接收缓冲区，用于从异步channel中读取数据包;
+        * A attachment：异步channel携带的附近，通知回调的时候作为入参使用;
+        * CompletionHandler<Integer,? super A> handler：接收回调通知的业务handler，如：ReadCompletionHandler.
+         */
         result.read(byteBuffer,byteBuffer,new ReadCompletionHandler(result));
     }
 
