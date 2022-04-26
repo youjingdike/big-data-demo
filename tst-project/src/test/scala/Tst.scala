4883bbbf7ec2676f043f85b6d64df383962bc774
xingqian <xingqian@kingsoft.com>
2022-04-26 10:25:20 +0800

    提交scala与Java集合的互转测试

--- tst-project/src/test/scala/Tst.scala
+++ tst-project/src/test/scala/Tst.scala
@@ -6,11 +6,20 @@ object Tst {
     val configs: Map[Any, Any] = Map("1"->"tst","2"->"tst1");
 
     val configMap: util.Map[Object, Object] = new util.HashMap(configs.asJava.asInstanceOf[util.Map[Object, Object]])
+    configMap.put("3","3")
     configMap.asScala.foreach(println(_))
 
+    println("@@@@@@@@")
 
     val configMap1: util.Map[String, String] = new util.HashMap(configs.map(x => x._1.toString -> x._2.toString).asJava)
+    configMap1.put("4","4")
     configMap1.asScala.foreach(println(_))
+    println("@@@@@@@@")
+
+    //该方式不能对map做修改，可使用第一种方式转换
+    val configMap2: util.Map[Object, Object] = configs.asJava.asInstanceOf[util.Map[Object, Object]]
+    //configMap2.put("5","5")
+    configMap2.asScala.foreach(println(_))
 
     val databaseList = new ArrayBuffer[String]
     /*def databaseList(databaseList: String*) = {
