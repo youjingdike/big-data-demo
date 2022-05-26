acf8f227b14cf3052a97c4a7a9c9ca36612e8c42
xingqian <xingqian@kingsoft.com>
2022-05-26 12:54:47 +0800

    提交redissontst

--- redisson-tst/src/test/java/com/xq/tst/AppTest.java
+++ redisson-tst/src/test/java/com/xq/tst/AppTest.java
@@ -3,6 +3,16 @@ package com.xq.tst;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RBucket;
+import org.redisson.api.RList;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
+import org.redisson.config.TransportMode;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.function.BiConsumer;
 
 /**
  * Unit test for simple App.
@@ -16,5 +26,32 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void tst() throws InterruptedException {
+        Config config = new Config();
+        config.setTransportMode(TransportMode.NIO);
+        config.useSingleServer()
+                .setPassword("123456")
+                //可以用"rediss://"来启用SSL连接
+                .setAddress("redis://192.168.0.100:6379");
+        RedissonClient redisson = Redisson.create(config);
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        RBucket<String> k1 = redisson.getBucket("k1",new StringCodec("utf-8"));
+        k1.getAsync().whenCompleteAsync((s, throwable) -> {
+            System.out.println(s);
+            countDownLatch.countDown();
+        });
+
+        RBucket<String> k2 = redisson.getBucket("k2",new StringCodec("utf-8"));
+        k2.getAsync().whenCompleteAsync((s, throwable) -> {
+            System.out.println(s);
+            countDownLatch.countDown();
+        });
+        System.out.println("wait");
+        countDownLatch.await();
+        System.out.println("end");
     }
 }
