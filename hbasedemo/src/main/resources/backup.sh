7254cdc6114e28f65fd592f8be2ce744a6725cf1
xingqian <xingqian@kingsoft.com>
2022-12-23 19:39:09 +0800

    提交hbase

--- hbasedemo/src/main/resources/backup.sh
+++ hbasedemo/src/main/resources/backup.sh
@@ -10,7 +10,7 @@ function statusCheck() {
     local status=$?
     if [ ${status} != 0 ];then
       echo "hbase hbck命令执行失败，请查看日志文件："${Shell_Path}/log/check/log.log
-      exit
+      exit 1
     fi
     echo "集群状态："
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -24,7 +24,7 @@ function statusCheck() {
     local nu=`grep 'inconsistencies detected' ${Shell_Path}/log/check/log.log | cut -d " " -f 1`
     if [ ${nu} != 0 ];then
       echo "检测出有"$nu"个表不一致，请查看日志文件："${Shell_Path}/log/check/log.log
-      exit
+      exit 1
     else
       echo "没有不一致的表"
     fi
@@ -37,7 +37,7 @@ function exportTableInfo() {
     local status=$?
     if [ ${status} != 0 ];then
       echo "list_namespace命令执行失败，请查看日志文件："${Shell_Path}/log/tabInfo/namespace.log
-      exit
+      exit 1
     fi
 
     sed -n '/NAMESPACE/,/row(s)/p'  ${Shell_Path}/log/tabInfo/namespace.log | sed -e '1d' | sed -e '$d' > ${Shell_Path}/log/tabInfo/all_namespace.txt
@@ -52,7 +52,7 @@ function exportTableInfo() {
        status=$?
        if [ ${status} != 0 ];then
          echo "list_namespace_tables '$ns'命令执行失败，请查看日志文件："${Shell_Path}/log/tabInfo/tables.log
-         exit
+         exit 1
        fi
        for tb in `sed -n '/TABLE/,/row(s)/p'  ${Shell_Path}/log/tabInfo/tables.log | sed -e '1d' | sed -e '$d'`
        do
@@ -96,7 +96,7 @@ function snapshot() {
       echo "源端已经存在的snapshot,如下："
       cat ${Shell_Path}/log/snapshot/snap_exits.txt
       echo "请删除重名的snapshot 或者 重新设置备份名称前缀"
-      exit
+      exit 1
     else
       echo "源端不存在要创建的snapshot..."
     fi
@@ -111,7 +111,7 @@ function snapshot() {
     do
       OLD_IFS="$IFS"
       IFS=":"
-      arr=($tb)
+      local arr=($tb)
       IFS="$OLD_IFS"
       ##echo "snapshot '$tb','qianyi-snap-${arr[0]}-${arr[1]}'" | hbase shell -n 1>>${Shell_Path}/log/snapshot/log.txt 2>&1
       echo "表"$tb",创建snapshot："qianyi-snap-${arr[0]}-${arr[1]}"的日志如下："
@@ -152,29 +152,73 @@ function dataCheck() {
 # -dc
 function printHelp() {
     echo "功能介绍："
-    echo "1.不输入参数或者输入：-all，执行整个流程"
-    echo "2.输入相应的参数，执行相应的功能："
-    echo "  a.状态检查，请输入：-sc"
-    echo "  b.导出表信息，请输入：-ex"
-    echo "  c.数据备份，请输入：-sn"
-    echo "  d.数据迁移，请输入：-tr"
-    echo "  e.数据校验，请输入：-dc"
-
+    echo "1.输入相应的参数，执行相应的功能："
+    echo "  a.状态检查，请输入参数：-sc hbase客户端的根目录 待检测HBASE集群的配置文件目录"
+    echo "  b.导出表信息，请输入参数：-ex hbase客户端的根目录 源端hbase的配置文件目录"
+    echo "  c.数据备份，请输入参数：-sn  hbase客户端的根目录 源端hbase的配置文件目录 要备份的表名称列表文件(全路径) 备份名称前缀"
+    echo "      说明：要备份的表名称列表文件，内容示例如下(格式：namespaceName:tableName，每一张表一行)："
+    echo "        default:table1"
+    echo "        namespace:table1"
+    echo "        namespace:table2"
+    echo "        注意：不备份hbase命名空间的表，如果添加了会剔除掉"
+    echo "  d.数据迁移，请输入参数：-tr hbase客户端的根目录 源端hbase的配置文件目录 目标端hbase的配置文件目录 目标端hbase对应hadoop集群的访问地址 迁移的snapshot的名称列表文件(全路径)"
+    echo "      说明：迁移的snapshot的名称列表文件，内容示例如下(格式：snapshotName，每一个备份名称一行)："
+    echo "        qianyi-snap-ns1-test1"
+    echo "        qianyi-snap-ns1-test2"
+    echo "  e.数据校验，请输入参数：-dc"
+    echo "  f.执行整个流程，请输入：-all hbase客户端的根目录 源端hbase的配置文件目录 目标端hbase的配置文件目录 目标端hbase对应hadoop集群的地址 备份名称前缀"
+    echo "2.参数说明如下："
+    echo "  hbase.root.dir     :  hbase客户端的根目录（可选：如果已经加入到path,可直接执行hbase命令)"
+    echo "  src.hbase.conf     :  源端hbase的配置文件目录/待检测HBASE集群的配置文件目录"
+    echo "  dst.hbase.conf     :  目标端hbase的配置文件目录"
+    echo "  dst.hadoop.uris    :  目标端hbase对应hadoop集群的地址"
+    echo "  snapshot.prefix    :  备份名称前缀（可选）"
+    echo "  snapshot.table.file:  要备份的表名称列表文件(全路径)"
+    echo "  snapshot.list.file :  迁移的snapshot的名称列表文件(全路径)"
+    echo "3.示例  :  "
+    echo "  sh backup.sh -all hbase.root.dir=/usr/hbase src.hbase.conf=/root/hbase/conf dst.hbase.conf=/root/hbase1/conf dst.hadoop.uris=hdfs://hdfs-ha snapshot.prefix=qianyi-"
 }
 
 function all(){
   statusCheck
   exportTableInfo
   snapshot
-  exit
   transfer
   dataCheck
 }
 
+function parseParam() {
+ for i in "$@"; do
+     #echo $i
+     OLD_IFS="$IFS"
+     IFS="="
+     local arr=($i)
+     IFS="$OLD_IFS"
+     #echo "arr的值为：${arr[*]}"
+     if [ "${#arr[@]}" -ge 2 ];then
+      local ind=${arr[0]}
+      #echo "ind@@@@@@:"$ind
+      if [ -n "$ind" ];then
+        paramMap[$ind]=${arr[1]}
+      fi
+     fi
+ done
+ echo "参数如下："
+ #echo "${!paramMap[@]}"
+ #echo "${paramMap[@]}"
+ for key in "${!paramMap[@]}";do
+  echo "key:"$key",value:"${paramMap[$key]}
+ done
+}
+
 param_count=$#
+#放获取到的参数
+declare -A paramMap
+parseParam "$@"
+exit
 if [ $# != 0 ]
 then
-  echo "参数个数为:$param_count, 不等于0,操作类型：$1"
+  echo "操作类型：$1"
   op_type=$1
   if [ "-sc" = ${op_type} ];then
     statusCheck
@@ -191,9 +235,8 @@ then
   elif [ "-h" = ${op_type} ];then
       printHelp
   else
-    echo "请输入正确的参数，输入-h 查看相应的参数信息"
+    echo "请输入正确的参数，输入-h 查看相应的功能参数信息"
   fi
 else
-   echo "参数个数为:$param_count, 等于0"
-   all
+   echo "请输入正确的参数，输入-h 查看相应的功能参数信息"
 fi
