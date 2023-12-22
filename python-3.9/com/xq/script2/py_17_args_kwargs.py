843e1084a8ee4b8a29c6f135247b6b6089c64aa9
xingqian <xingqian@kingsoft.com>
2023-12-22 18:01:46 +0800

    提交python3的方法参数的测试

--- python-3.9/com/xq/script2/py_17_args_kwargs.py
+++ python-3.9/com/xq/script2/py_17_args_kwargs.py
@@ -2,7 +2,8 @@
 # coding=utf-8
 
 # 自定义函数，包含*args和**kwargs
-# 注意：这里的args,kwargs是可以换成其他名称的，不是固定的
+# 注意：这里函数定义时的args,kwargs是可以换成其他名称的，不是固定的
+# 注意：函数调用时的*和**是对可迭代对象进行拆分
 def func(argument, *args, **kwargs):
     print("func !!!!!!!!")
     print("argument:" + str(argument))
@@ -21,10 +22,7 @@ dict = {
 func(1, 2, a=3)
 # 调用函数，传入参数
 # 注意位置参数和关键字参数的顺序
-
-# 这个属于关键字参数调用
 func(*tuple, **dict)
-# 这个属于位置参数调用
 func(tuple, dict)
 
 print("###########")
@@ -43,8 +41,8 @@ def func1(*args, argument, **kwargs):
 func1(1, 2, 3, 2, 3, 3, argument=2, a=3)
 # 调用函数，传入参数
 # 注意位置参数和关键字参数的顺序
-func1(*tuple, argument=5, **dict)  # 这个属于关键字参数调用
-func1(*tuple, 50,argument=5, **dict)  # 这个属于关键字参数调用
+func1(*tuple, argument=5, **dict)
+func1(*tuple, 50,argument=5, **dict)
 # func1(*tuple, **dict)  # 这个报错
 func1(tuple,51, argument=9, **dict)
 func1(tuple,52, argument=9)
@@ -63,11 +61,9 @@ func2(1, 2, 3, 2, 3, 3, a=3)
 func2(1, 2, 3, 2, 3, 3, argument=2, a=3)
 # 调用函数，传入参数
 # 注意位置参数和关键字参数的顺序
-# 这个属于关键字参数调用
 func2(*tuple, argument=5, **dict)
 func2(*tuple, argument=5)
 # func2(*tuple, argument=5,dict) # 这个会报错
-# 这个属于位置参数调用
 func2(tuple, 9, dict)  # 这个会把所有的参数都传给args
 
 print("*************************")
@@ -82,12 +78,46 @@ func3(1, 2, 3, 2, 3, 3, a=3)
 # func3(1, 2, 3, 2, 3, 3, argument=50,a=3) # 这个运行会报错
 # 调用函数，传入参数
 # 注意位置参数和关键字参数的顺序
-# 这个属于关键字参数调用
 func3(1,*tuple, **dict)
 func3(*tuple, **dict)
 func3(tuple, **dict)
 func3(tuple, dict)
 func3(argument=5,**dict)
 # func3(*tuple, argument=5,dict) # 这个会报错
-# 这个属于位置参数调用
 func3(tuple, 9, dict)  # 这个会把后两个的参数都传给args
+print("*******func4*****************")
+def func4(**kwargs):
+    print("kwargs:"+str(kwargs))
+
+func4(**dict)
+# func4(dict)
+func4(a=3)
+
+print("***********(*/**)***********")
+
+print("a", "b", "c")
+print(*("a", "b", "c"))             # 元组
+print(*["a", "b", "c"])             # 列表
+print(*{"a": 1, "b": 2, "c": 3})    # 注意：对字典拆解时只拆解key
+print(*"abc")                       # 注意：字符串也是可迭代对象
+print(*{"a", "b", "c"})             # 注意的是，集合不太一样，因为集合是无序的，所以print(*{"a", "b", "c"})的时候"a", "b", "c"可能是乱序的。
+
+a = *range(3),
+b = *range(3), 3
+c = [*range(3)]
+d = {*range(3)}
+e = {*{'y': 1}}     # 只对key进行拆解
+f = {**{'y': 1}}    # 对key和value都进行了拆解
+
+
+print(a)  # output: (0,1,2)
+print(b)  # output: (0,1,2,3)
+print(c)  # output: [0,1,2]
+print(d)  # output: {0,1,2}
+print(e)  # output: {'y'}
+print(f)  # output: {'y':1}
+
+a, *b, c = {1, 2, 3, 4, 5, 6}
+print(a)
+print(b)
+print(c)
