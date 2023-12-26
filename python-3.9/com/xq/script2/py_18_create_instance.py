9ba9288f027decf6b3893359cea272c7fed9c970
xingqian <xingqian@kingsoft.com>
2023-12-26 18:04:24 +0800

    提交python3学习的测试

--- python-3.9/com/xq/script2/py_18_create_instance.py
+++ python-3.9/com/xq/script2/py_18_create_instance.py
@@ -2,24 +2,25 @@
 # coding=utf-8
 
 class MyClass:
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        print(self.__class__.__name__,self.x,self.y)
+        print(self.__class__.__name__, self.x, self.y)
+
 
 if __name__ == '__main__':
-    my_class = type("MyClass",(),{"x":1,"y":2})
+    my_class = type("MyClass", (), {"x": 1, "y": 2})
     my_ins = my_class()
     # print(my_ins)
     # print(my_ins.x,my_ins.y)
     print("@@@@@")
 
     clsName = globals().get("MyClass")
-    obj = clsName(1,2)
+    obj = clsName(1, 2)
     # print(obj.x,obj.y)
 
     print("!!!!!")
     module = __import__("py_18_create_instance")
-    cls_name = getattr(module,"MyClass")
-    obj_ins = cls_name(1,2)
+    cls_name = getattr(module, "MyClass")
+    obj_ins = cls_name(1, 2)
     # print(obj_ins.x,obj_ins.y)
